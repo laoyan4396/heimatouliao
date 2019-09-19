7a8abf497755580b96df5d683abe5f5c058b6826
laoyan4396 <1149006907@qq.com>
2019-09-19 11:55:49 +0800

    黑马头条PC-初始化项目-整理目录-准备资源

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+// import Home from './views/home/index.vue'// 路由集组件
+import Home from './views/home/'
 Vue.use(Router)
 
 export default new Router({
@@ -10,14 +10,14 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
+    // 按需加载{
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    // }
   ]
 })

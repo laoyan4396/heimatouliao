654569cddf58efc4d04b7c853f257503d6fa4f6a
laoyan4396 <1149006907@qq.com>
2019-09-26 21:48:57 +0800

    黑马头条PC-主页模块-左侧导航菜单

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,7 @@
 <template>
-<!-- 定义一个容器 -->
-  <el-container>
- <!-- 先放置一个左侧的容器 -->
-  <el-aside>左侧容器</el-aside>
-  <!-- 右侧大容器 -->
-  <el-container>
-    <el-header>头部</el-header>
-    <el-main>
-      <!-- 中补区域 -->
-       <router-view></router-view>
-    </el-main>
-    <!-- 二级路由容器 -->
-     <router-view></router-view>
-  </el-container>
-  </el-container>
+  <div class="home">
+   我是主页
+  </div>
 </template>
 
 <script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="login">
-      我是主页
-  </div>
+
+<!-- 定义一个容器 -->
+  <el-container>
+ <!-- 先放置一个左侧的容器 -->
+  <el-aside>
+<!-- 左侧导航组件 -->
+  <layout-aside></layout-aside>
+  </el-aside>
+  <!-- 右侧大容器 -->
+  <el-container>
+    <el-header>头部</el-header>
+    <el-main>
+      <!-- 中补区域 -->
+       <router-view></router-view>
+    </el-main>
+    <!-- 二级路由容器 -->
+     router-view
+  </el-container>
+  </el-container>
+
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
